ef5e7a0bef93dec760c97660103465cc70cb5359
ваше имяwlad999 <wladmail25@gmeil.com>
2019-05-01 15:43:17 +0300

    07 home

--- JavaScript/MAX!!!!Bootcamp11/mod.7-16.04M/js/index.js
+++ JavaScript/MAX!!!!Bootcamp11/mod.7-16.04M/js/index.js
@@ -358,7 +358,7 @@ last.setAttribute('style','color:red; font-size: 40px')
 // ===================================
 
 
-// ------------------------------------------------------------------
+// --------------------------HOME WORK 07----------------------------------------
 /*1. Модифицируйте готовую функцию createPostCard() из задания 
     номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так, 
     чтобы она принимала объект post с данными для заполнения полей 
@@ -370,26 +370,26 @@ last.setAttribute('style','color:red; font-size: 40px')
     массив DOM-элементов всех постов.
     
   3. Повесьте все посты в какой-то уже существующий DOM-узел.*/
-//     const posts = [
-//     {
-//       img: "https://placeimg.com/400/150/arch",
-//       title: "Post title 1",
-//       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
-//       link: 'link-1.com'
-//     },
-//     {
-//       img: "https://placeimg.com/400/150/nature",
-//       title: "Post title 2",
-//       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
-//       link: 'link-2.com'
-//     },
-//     {
-//       img: "https://placeimg.com/400/150/animals",
-//       title: "Post title 3",
-//       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
-//       link: 'link-3.com'
-//     }
-//   ];
+    const posts = [
+    {
+      img: "https://placeimg.com/400/150/arch",
+      title: "Post title 1",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+      link: 'link-1.com'
+    },
+    {
+      img: "https://placeimg.com/400/150/nature",
+      title: "Post title 2",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+      link: 'link-2.com'
+    },
+    {
+      img: "https://placeimg.com/400/150/animals",
+      title: "Post title 3",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+      link: 'link-3.com'
+    }
+  ];
 
   
 // function createMovieCard(el){
@@ -440,8 +440,8 @@ last.setAttribute('style','color:red; font-size: 40px')
 // /*view.append('markup'); не работает*/
 // view.insertAdjacentHTML('beforeend', markup);
 // -----------------------------------------
-// let view = document.querySelector('.movie')
-// posts.forEach(el=>view.insertAdjacentHTML('beforeend', `<img src="${el.img}" style="width: 300px"><h2 class="movie__title">${el.title}</h2><p class="movie__description">${el.text}</p><a href ='${el.link}'>${el.link}</a>`))
+let view = document.querySelector('.movie')
+posts.forEach(el=>view.insertAdjacentHTML('beforeend', `<img src="${el.img}" style="width: 300px"><h2 class="movie__title">${el.title}</h2><p class="movie__description">${el.text}</p><a href ='${el.link}'>${el.link}</a>`))
 // ---------------------------------------------
 
 
