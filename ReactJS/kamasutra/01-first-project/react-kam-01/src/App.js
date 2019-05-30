9d360b9925d174abd00894514b9768b6f5b3a5d5
ваше имяwlad999 <wladmail25@gmeil.com>
2019-05-30 10:33:26 +0300

    20.lessson

--- ReactJS/kamasutra/01-first-project/react-kam-01/src/App.js
+++ ReactJS/kamasutra/01-first-project/react-kam-01/src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
+import Music from './components/Music/Music';
 import {
   BrowserRouter,
   Route,
@@ -11,13 +14,16 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter> {/*подключаем тег <BrowserRouter>*/}
       <div className="app-wrapper">
         <Header />
         <NavBar />
         <div className="app-wrapper-content">
           <Route path="/Dialogs" component={Dialogs} />
           <Route path="/Profile" component={Profile} />
+          <Route path="/News" component={News} />
+          <Route path="/Settings" component={Settings} />
+          <Route path="/Musik" component={Music} />
         </div>
       </div>
     </BrowserRouter>
