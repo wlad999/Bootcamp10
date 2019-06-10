4510f55f9500244a92e5e37e94cd5aecfe734b15
ваше имяwlad999 <wladmail25@gmeil.com>
2019-06-10 11:31:44 +0300

    ks-27

--- ReactJS/kamasutra/01-first-project/react-kam-01/src/App.js
+++ ReactJS/kamasutra/01-first-project/react-kam-01/src/App.js
@@ -14,13 +14,22 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter> {/*подключаем тег <BrowserRouter>*/}
+    <BrowserRouter>
+      {' '}
+      {/*подключаем тег <BrowserRouter>*/}
       <div className="app-wrapper">
         <Header />
         <NavBar />
         <div className="app-wrapper-content">
-          <Route path="/Dialogs" component={Dialogs} />
-          <Route path="/Profile" component={Profile} />
+          {/* <Route path="/Dialogs" component={Dialogs} />
+          <Route path="/Profile" component={Profile} /> */}
+          {/* при совпадении пути path="/Profile" -  Route вызывает component={Profile}. 
+          В данном случае {Profile} просто название компоненты,
+        поэтому мы не можеи туда прокинуть props, выходим из положения путём использования атрибута "render", 
+        куда запихуем анонимную функцию, 
+        который отрисовывает компоненту, а в неё уже можно закинуть props*/}
+          <Route path="/Dialogs" render={() => <Dialogs />} />
+          <Route path="/Profile" render={() => <Profile />} />
           <Route path="/News" component={News} />
           <Route path="/Settings" component={Settings} />
           <Route path="/Musik" component={Music} />
