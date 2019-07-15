15dde23dc86b8eb29933abc795522864967dde11
ваше имяwlad999 <wladmail25@gmeil.com>
2019-07-15 16:20:57 +0300

    52 - React - Side Effect

--- ReactJS/kamasutra/01-first-project/react-kam-01/src/components/Users/Users.jsx
+++ ReactJS/kamasutra/01-first-project/react-kam-01/src/components/Users/Users.jsx
@@ -4,46 +4,19 @@ import * as Axios from 'axios';
 import userPhoto from '../../assets/images/users.png';
 
 let Users = props => {
-  if (props.users.length === 0) {
-    Axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
-      response => {
-        // debugger;
-        props.setUsers(response.data.items);
-      },
-    );
-    // props.setUsers([
-    //   {
-    //     id: 1,
-    //     photoUrl:
-    //       'https://steamuserimages-a.akamaihd.net/ugc/963095678570779142/678F6DDE61B17A1AA32739F4D31B128B5B5BF6D1/',
-    //     followed: false,
-    //     fullName: 'Dmitry',
-    //     status: 'i am a Boss',
-    //     location: { city: 'Minsk', country: 'Belarus' },
-    //   },
-    //   {
-    //     id: 2,
-    //     photoUrl:
-    //       'https://www.meme-arsenal.com/memes/12be6fef3f4ab64e65e4f69a19b9f64d.jpg',
-    //     followed: true,
-    //     fullName: 'Alex',
-    //     status: 'i am a Boss too',
-    //     location: { city: 'Moscow', country: 'Russia' },
-    //   },
-    //   {
-    //     id: 3,
-    //     photoUrl:
-    //       'https://steamuserimages-a.akamaihd.net/ugc/933814008881152016/838D9568CF06E89568DD6D8D58A61291110336C5/',
-    //     followed: false,
-    //     fullName: 'Andrew',
-    //     status: 'i am a more Boss',
-    //     location: { city: 'Kiev', country: 'Ukraine' },
-    //   },
-    // ]);
-  }
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      Axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
+        response => {
+          props.setUsers(response.data.items);
+        },
+      );
+    }
+  };
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {props.users.map(u => (
         <div key={u.id}>
           <span>
