b4fd73c0ff8dab8f365b658db12c11efee4884bc
ваше имяwlad999 <wladmail25@gmeil.com>
2019-07-15 18:40:30 +0300

    53 - (extends React.Component)

--- ReactJS/kamasutra/01-first-project/react-kam-01/src/components/Users/Users.jsx
+++ ReactJS/kamasutra/01-first-project/react-kam-01/src/components/Users/Users.jsx
@@ -3,64 +3,66 @@ import styles from './users.module.css';
 import * as Axios from 'axios';
 import userPhoto from '../../assets/images/users.png';
 
-let Users = props => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      Axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
-        response => {
-          props.setUsers(response.data.items);
-        },
-      );
-    }
-  };
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    alert('ALERT');
 
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      {props.users.map(u => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={u.photos.small != null ? u.photos.small : userPhoto}
-                alt=""
-                className={styles.userPhoto}
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+    Axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
+      response => {
+        this.props.setUsers(response.data.items);
+      },
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <button onClick={this.getUsers}>Get Users</button> */}
+        {this.props.users.map(u => (
+          <div key={u.id}>
             <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
+              <div>
+                <img
+                  src={u.photos.small != null ? u.photos.small : userPhoto}
+                  alt=""
+                  className={styles.userPhoto}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              <div>{'u.location.counrty'}</div>
-              <div>{'u.location.city'}</div>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{'u.location.counrty'}</div>
+                <div>{'u.location.city'}</div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
-
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 export default Users;
