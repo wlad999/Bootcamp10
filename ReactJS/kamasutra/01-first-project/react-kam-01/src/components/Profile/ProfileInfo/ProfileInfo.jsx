b2453ec84f8c1761b0963d2f2a8fffe3f816993e
ваше имяwlad999 <wladmail25@gmeil.com>
2019-07-22 18:46:25 +0300

    59 finish

--- ReactJS/kamasutra/01-first-project/react-kam-01/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ ReactJS/kamasutra/01-first-project/react-kam-01/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import userPhoto from '../../../assets/images/users.png';
 import Preloader from '../../common/preloader/Preloader';
+import lookingForAJob from '../../../assets/images/looking-for-a-job-500x286.jpg';
+import noNeedAJob from '../../../assets/images/no.png';
+import twitter from '../../../assets/images/twitter_PNG32.png';
+import fb from '../../../assets/images/FB.svg';
+import git from '../../../assets/images/GH.png';
 
 const ProfileInfo = props => {
   if (!props.propfile) {
     return <Preloader />;
   }
+  // debugger;
   return (
     <>
       <div>
@@ -22,6 +28,26 @@ const ProfileInfo = props => {
           // }
           alt=""
         />
+        <div>aboutMe: {props.propfile.aboutMe}</div>
+        <div>
+          {/* lookingForAJob: */}
+          {props.propfile.lookingForAJob ? (
+            <img src={lookingForAJob} alt="" width="100px" />
+          ) : (
+            <img src={noNeedAJob} alt="" />
+          )}
+        </div>
+        <div>
+          <a href={props.propfile.contacts.twitter}>
+            <img src={twitter} alt="" width="50px" />
+          </a>
+          <a href={'https://' + props.propfile.contacts.facebook}>
+            <img src={fb} alt="" width="50px" />
+          </a>
+          <a href={'https://' + props.propfile.contacts.github}>
+            <img src={git} alt="" width="50px" />
+          </a>
+        </div>
         ava + descripyion
       </div>
     </>
