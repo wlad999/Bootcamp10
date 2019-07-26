614bae19f2197cede2642c1a7b31176e896caaa4
ваше имяwlad999 <wladmail25@gmeil.com>
2019-07-26 18:34:48 +0300

    63 - DAL, axios.create - start object userAPI

--- ReactJS/kamasutra/01-first-project/react-kam-01/src/api/api.js
+++ ReactJS/kamasutra/01-first-project/react-kam-01/src/api/api.js
@@ -1,9 +1,21 @@
 import * as Axios from 'axios';
+// const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
+const instance = Axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': '635d4a98-5c9c-4ce7-b29e-636f275b6bb7',
+  },
+});
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-  return Axios.get(
-    `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count = ${pageSize}`,
-    {
-      withCredentials: true,
-    },
-  ).then(response => response.data);
+  return instance
+    .get(`users?page=${currentPage}&count = ${pageSize}`)
+    .then(response => response.data);
 };
+
+// export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+//   return instance
+//     .get(baseUrl + `follow?page=${currentPage}&count = ${pageSize}`)
+//     .then(response => response.data);
+// };
